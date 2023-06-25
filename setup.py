3dbbbb52aee07d066e693d9aca20a9500262e491
bsanchez <mrsipan@users.noreply.github.com>
2023-06-25 15:03:16 -0400

    Make a setuptools package

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='cartoongan',
+    version='0.0.1',
+    description='CartoonGAN style transfer for Hayao style.',
+    author='Filip Andersson, Simon Arvidsson',
+    packages=['cartoongan'],
+    package_dir={'cartoongan': '.'},
+    license='MIT'
+    )
